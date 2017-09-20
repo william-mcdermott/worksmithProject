507b4514caa9e10bb3ea8d8961478bb8f3eba619
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-09-19 22:46:24 -0500

    clear input field when value submitted

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,15 +7,18 @@ app.controller('changeCalculatorController', function($scope) {
   };
   $scope.invalidInput = false;
   $scope.calculateChange = function (amount) {
+    $scope.amount = '';
     console.log(parseInt(amount));
-    if (amount == Number(amount)) {
+    amount = Math.round(amount * 100) / 100;
+    if (amount == Number(amount) && Number(amount) > 0) {
       $scope.invalidInput = false;
       $scope.change.quarters = Math.floor(amount / 0.25);
       amount = ((amount * 100) % 25) / 100;
+      console.log(amount);
       $scope.change.dimes = Math.floor(amount / 0.1);
       amount = ((amount * 100) % 10) / 100;
       $scope.change.nickels = Math.floor(amount / 0.05);
-      $scope.change.pennies = parseInt((amount * 100) % 5);
+      $scope.change.pennies = Math.round((amount * 100) % 5);
     } else {
       $scope.invalidInput = true;
     }
