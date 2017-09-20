36797de8e7af47d9619829ef9206650d68818991
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-09-19 21:29:28 -0500

    Add some validation

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,7 +1,19 @@
 app.controller('changeCalculatorController', function($scope) {
-	$scope.quarters = 25
-	$scope.dimes = 10
-	$scope.nickels = 5
-	$scope.pennies = 1
+	$scope.change = {
+    quarters: 25,
+	  dimes: 10,
+    nickels: 5,
+    pennies: 1
+  };
+  $scope.invalidInput = false;
+  $scope.calculateChange = function (amount) {
+    console.log(parseInt(amount));
+    if (amount == Number(amount)) {
+      $scope.invalidInput = false;
+      $scope.change.quarters = amount;
+    } else {
+      $scope.invalidInput = true;
+    }
+  };
 
 });
